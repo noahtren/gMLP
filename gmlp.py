4fb9fd06ce0797c03a01b22fa9fb815fd70cacb6
Noah Trenaman <noahtren@gmail.com>
2021-05-20 23:33:45 -0400

    updates

--- gmlp.py
+++ gmlp.py
@@ -1,7 +1,12 @@
+import functools
+import operator
+
+import jax
 import jax.numpy as np
 import jax.random as random
 
 import flax.linen as nn
+import flax
 
 
 class SpatialGatingUnit(nn.Module):
@@ -12,13 +17,13 @@ class SpatialGatingUnit(nn.Module):
     dims = v.shape[1:-1]
     axes = np.arange(v.ndim)[1:-1]
     v = np.moveaxis(v, [0, v.ndim - 1, *axes], np.arange(v.ndim))
-    v = nn.DenseGeneral(
+    general = nn.DenseGeneral(
         features=dims,
-        axis=np.arange(v.ndim)[2:],
-        batch_dims=[0, 1],
+        axis=tuple(np.arange(v.ndim)[2:]),
         kernel_init=nn.initializers.variance_scaling(
             0.1, 'fan_in', 'truncated_normal'),  # near-zero projection matrix
-        bias_init=nn.initializers.ones)(v)
+        bias_init=nn.initializers.ones)
+    v = general(v)
     v = np.moveaxis(v, np.arange(v.ndim), [0, v.ndim - 1, *axes])
     return u * v
 
@@ -43,7 +48,30 @@ class gMLPBlock(nn.Module):
     return x
 
 
+class gMLPModel(nn.Module):
+  ffn_dim: int
+  model_dim: int
+  num_blocks: int
+
+  @nn.compact
+  def __call__(self, x):
+    x = nn.Dense(name='embedding', features=self.model_dim)(x)
+    for i in range(self.num_blocks):
+      x = gMLPBlock(ffn_dim=self.ffn_dim, model_dim=self.model_dim)(x)
+    return x
+
+
+tiny_settings = {'ffn_dim': 768, 'model_dim': 128, 'num_blocks': 30}
+
 if __name__ == "__main__":
   key = random.PRNGKey(2)
   x = random.normal(key, shape=[8, 12, 18, 100])
-  model_state = SpatialGatingUnit().init(key, x)
+  model = gMLPModel(**tiny_settings)
+  model_state = model.init(key, x)
+  y = model.apply(model_state, x)
+  print(
+      json.dumps(jax.tree_map(np.shape,
+                              flax.core.unfreeze(model_state['params'])),
+                 indent=2))
+  num_params = functools.reduce(
+      operator.add, map(np.size, jax.tree_leaves(model_state['params'])))
